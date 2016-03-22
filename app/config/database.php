2f45c9e1d718cda0885c79033ded2f5501b32152
Adit Shah <ashah@kairasoftware.com>
2016-03-22 17:15:44 +0530

    db changes

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => '54.175.123.119',
 			'database'  => 'forge',
 			'username'  => 'forge',
 			'password'  => 'kll40glEOoeQrZ1nA4YC',
--- app/config/local/database.php
+++ app/config/local/database.php
@@ -22,7 +22,7 @@ return array(
 
         'mysql' => array(
             'driver'    => 'mysql',
-            'host'      => 'localhost',
+            'host'      => '54.175.123.119',
             'database'  => 'forge',
             'username'  => 'forge',
             'password'  => 'kll40glEOoeQrZ1nA4YC',
